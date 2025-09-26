99af167c0cfc239b952375e33190190b3365965d
khaleelul Varis <khaleelulvaris@gmail.com>
2025-09-26 23:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (73.46%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function climbStairs(n) {
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+
+  let dp = new Array(n + 1);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+}
+
+console.log(climbStairs(5)); 
