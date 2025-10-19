c6906e61b4d1e150913a4587930f7bbea8840d75
khaleelul Varis <khaleelulvaris@gmail.com>
2025-10-19 17:14:23 +0530

    Time: 54 ms (59.77%) | Memory: 54.8 MB (32.99%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,13 +4,10 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    try{
-        const one=await promise1
-        const two=await promise2
-        return one+two
-    }catch(err){
-        console.log(err)
-    }
+    const one=await promise1;
+    const two =await promise2;
+    return one+two
+    
 };
 
 /**
