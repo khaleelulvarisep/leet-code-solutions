9002191ad6001881390c7f1ce9945d6a5987f411
khaleelul Varis <khaleelulvaris@gmail.com>
2025-10-19 17:17:28 +0530

    Time: 0 ms (100.00%) | Memory: 58.1 MB (45.51%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if (nums.length === 0) return 0;
     let i = 0; 
     for (let j = 1; j < nums.length; j++) {
         if (nums[j] !== nums[i]) {
