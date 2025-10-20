21ad3d09b2be691388d88863355ff530503b6d75
khaleelul Varis <khaleelulvaris@gmail.com>
2025-10-20 06:24:01 +0530

    Time: 263 ms (5.15%) | Memory: 54.3 MB (88.32%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,7 +5,7 @@
  */
 var twoSum = function(nums, target) {
     let number=[]
-    for(let i=0;i<=nums.length-1;i++){
+    for(let i=0;i<=nums.length;i++){
         for(let j=0;j<=nums.length;j++){
             if(j==i){
                 continue;
