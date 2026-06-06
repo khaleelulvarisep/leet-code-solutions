d71870867333eeda4d5700764d6fc5de06897b0c
khaleelul Varis <khaleelulvaris@gmail.com>
2026-06-06 23:38:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (95.36%) - LeetSync

--- 4290-valid-elements-in-an-array/valid-elements-in-an-array.py
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.py
@@ -5,19 +5,19 @@ class Solution:
         left_max = [0] * n
         right_max = [0] * n
 
-        # build left max
+        
         max_val = float('-inf')
         for i in range(n):
             left_max[i] = max_val
             max_val = max(max_val, nums[i])
 
-        # build right max
+        
         max_val = float('-inf')
         for i in range(n - 1, -1, -1):
             right_max[i] = max_val
             max_val = max(max_val, nums[i])
 
-        # collect result
+       
         res = []
         for i in range(n):
             if i == 0 or i == n - 1:
