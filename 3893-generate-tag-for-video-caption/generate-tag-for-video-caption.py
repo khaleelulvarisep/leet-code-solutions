23cad8a34727dd037c9c902e5b74c1fcc44ff65d
khaleelul Varis <khaleelulvaris@gmail.com>
2026-06-25 21:27:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (88.94%) - LeetSync

---
+++ 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.py
@@ -0,0 +1,39 @@
+class Solution:
+    def generateTag(self, caption: str) -> str:
+        
+        cap=caption.strip()
+        result='#'
+        if cap:
+            
+            result+=cap[0].lower()
+ 
+        flag=False
+        for i in range(1,len(cap)):
+
+            if cap[i]==' ':
+                flag=True
+                continue
+            if flag:
+                result+=cap[i].upper()  
+                flag=False
+            else:
+                result+=cap[i].lower()    
+
+        if len(result)>100:
+            return result[:100]
+        else:
+            return result    
+
+
+
+
+
+           
+
+
+           
+              
+
+            
+            
+        
